66cf1b813655098856ce361b06ee576d32bc4c87
Nikki-Rees <nikki-rees@outlook.com>
2020-10-08 12:37:49 +1100

    deploying

--- server.js
+++ server.js
@@ -15,9 +15,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts_db", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 })
 
